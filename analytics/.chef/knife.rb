2ad3ab954748d243431c599fadbea9bed475c1f1
Thom May <thom@chef.io>
2015-06-03 19:10:24 +0100

    upload rules and notifications

--- analytics/.chef/knife.rb
+++ analytics/.chef/knife.rb
@@ -7,5 +7,7 @@ client_key               "#{current_dir}/analytics.pem"
 validation_client_name   "analytics-validator"
 validation_key           "#{current_dir}/analytics-validator.pem"
 chef_server_url          "https://192.168.56.100/organizations/analytics"
+analytics_server_url     "https://192.168.56.101/organizations/analytics"
 syntax_check_cache_path  "#{current_dir}/syntax_check_cache"
 ssl_verify_mode :verify_none
+cookbook_path           "#{current_dir}/../cookbooks"
